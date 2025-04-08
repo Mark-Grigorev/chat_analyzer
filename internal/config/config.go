aaeec97c53d372978ebaf3c0884b800e890ce0d4
Mark Grigorev <107909196+Mark-Grigorev@users.noreply.github.com>
2025-04-08 11:59:35 +0600

    add tests & mini refactoring

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,12 +1,29 @@
 package config
 
 import (
-	"github.com/Mark-Grigorev/chat_analyzer/internal/model"
 	"github.com/kelseyhightower/envconfig"
 )
 
-func Read() *model.Config {
-	var config model.Config
+type Config struct {
+	Debug          string `envconfig:"DEBUG" required:"true"`
+	LLMConfig      LLMConfig
+	TelegramConfig TelegramConfig
+}
+
+type LLMConfig struct {
+	URL         string  `envconfig:"LLM_URL" required:"true"`
+	Token       string  `envconfig:"LLM_TOKEN" required:"true"`
+	Model       string  `envconfig:"LLM_MODEL" required:"true"`
+	Temperature float64 `envconfig:"LLM_TEMPERATURE" required:"true"`
+}
+
+type TelegramConfig struct {
+	Token   string  `envconfig:"TG_TOKEN" required:"true"`
+	ChatIDS []int64 `envconfig:"TG_CHAT_IDS" required:"true"`
+}
+
+func Read() *Config {
+	var config Config
 	envconfig.Process("", &config)
 	return &config
 }
